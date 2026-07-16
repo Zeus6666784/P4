1c288ca47930834cbfeb9c01c8b68e5b58703a75
LAVKUSH VISHWAKARMA <lavkvishmvlu@gmail.com>
2026-07-16 15:48:12 +0530

    Add project files.

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(P4.Startup))]
+namespace P4
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
